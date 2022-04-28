c20d8707175153cd030423ca1618394b14bfbe0b
Jamie <c00208743@itcarlow.ie>
2022-04-28 23:25:14 +0100

    Fourth Puzzle

---
+++ Python/Python Division.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+    intDiv = (a // b)
+    print (intDiv)
+    
+    floatDiv = (a / b)
+    print (floatDiv)
